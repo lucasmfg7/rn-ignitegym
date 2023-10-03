904ac50d2232bd6dd9516ba691b300c57790c261
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-10-03 11:45:04 -0300

    Customizando o tema default

--- App.tsx
+++ App.tsx
@@ -6,18 +6,19 @@ import {
 } from '@expo-google-fonts/roboto'
 import { NativeBaseProvider } from 'native-base'
 import { Loading } from '@components/Loading'
+import { THEME } from './src/theme'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
-      {!fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   )
 }
--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,8 +3,8 @@ import { Center, Spinner } from 'native-base'
 
 export const Loading = () => {
   return (
-    <Center flex={1}>
-      <Spinner />
+    <Center flex={1} bg='gray.700'>
+      <Spinner color='green.500' />
     </Center>
   )
 }
