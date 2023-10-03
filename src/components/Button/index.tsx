ddff9713e8b9e9451e6d4975b0f1f3d43f1423ae
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-10-03 16:48:35 -0300

    Restringindo as variantes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,21 +2,28 @@ import { Button as NativeBaseButton, IButtonProps, Text } from 'native-base'
 
 type Props = IButtonProps & {
   title: string
+  variant?: 'solid' | 'outline'
 }
 
-export const Button = ({ title, ...props }: Props) => {
+export const Button = ({ title, variant = 'solid', ...props }: Props) => {
   return (
     <NativeBaseButton
       w='full'
       h={14}
-      bg='green.700'
+      bg={variant === 'outline' ? 'transparent' : 'green.700'}
+      borderWidth={variant === 'outline' ? 2 : 0}
+      borderColor='green.700'
       rounded='sm'
       _pressed={{
-        bg: 'green.500',
+        bg: variant === 'outline' ? 'gray.500' : 'green.500',
       }}
       {...props}
     >
-      <Text color='white' fontFamily='heading' fontSize='sm'>
+      <Text
+        color={variant === 'outline' ? 'green.500' : 'white'}
+        fontFamily='heading'
+        fontSize='sm'
+      >
         {title}
       </Text>
     </NativeBaseButton>
