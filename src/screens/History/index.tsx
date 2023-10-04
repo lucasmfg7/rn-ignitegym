c771a9225e330afaad05c4e97548a96afd241a1c
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-10-04 16:33:19 -0300

    Criando a estruturas das interfaces do App

---
+++ src/screens/History/index.tsx
@@ -0,0 +1,10 @@
+import { Center, Text } from 'native-base'
+import React from 'react'
+
+export const History = () => {
+  return (
+    <Center flex={1}>
+      <Text>History</Text>
+    </Center>
+  )
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Center, Text } from 'native-base'
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <Center flex={1}>
+      <Text>Home</Text>
+    </Center>
+  )
+}
