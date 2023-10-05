530bdb7e2013f17d83373b4f5632c04d105b0b07
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-10-05 17:09:55 -0300

    Estilizando menu ativo e inativos

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,15 +2,15 @@ import {
   BottomTabNavigationProp,
   createBottomTabNavigator,
 } from '@react-navigation/bottom-tabs'
-import { Exercise } from '@screens/Exercise'
-import { History } from '@screens/History'
 import { Home } from '@screens/Home'
+import { History } from '@screens/History'
 import { Profile } from '@screens/Profile'
+import { Exercise } from '@screens/Exercise'
+import { useTheme } from 'native-base'
 
 import HomeSvg from '@assets/home.svg'
 import HistorySvg from '@assets/history.svg'
 import ProfileSvg from '@assets/profile.svg'
-import { useTheme } from 'native-base'
 
 type AppRoutes = {
   home: undefined
@@ -24,12 +24,19 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export const AppRoutes = () => {
-  const { sizes } = useTheme()
+  const { sizes, colors } = useTheme()
 
   const iconSize = sizes['6']
 
   return (
-    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.green[500],
+        tabBarInactiveTintColor: colors.gray[200],
+      }}
+    >
       <Screen
         name='home'
         component={Home}
