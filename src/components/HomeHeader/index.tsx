f0fb0b3a2f0e94382346ec8647f265e9973f83a1
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-10-05 18:06:17 -0300

    Componente UserPhoto

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,8 +1,15 @@
+import { UserPhoto } from '@components/UserPhoto'
 import { HStack, Heading, Text, VStack } from 'native-base'
 
 export const HomeHeader = () => {
   return (
     <HStack bg='gray.600' pt={16} pb={5} px={8} alignItems='center'>
+      <UserPhoto
+        size={16}
+        source={{ uri: 'https://avatars.githubusercontent.com/u/95872678?v=4' }}
+        alt='Imagem do usuário'
+        mr={4}
+      />
       <VStack>
         <Text color='gray.100' fontSize='md'>
           Olá,
