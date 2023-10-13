82371078605cdb69d6a57fb854ea7f08dfa98c00
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-10-13 11:57:31 -0300

    Efeito Skeleton

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,21 +1,35 @@
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
-import { Center, ScrollView, VStack } from 'native-base'
-import React from 'react'
+import { Center, ScrollView, Skeleton, VStack } from 'native-base'
+import { useState } from 'react'
+
+const PHOTO_SIZE = 33
 
 export const Profile = () => {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
     <VStack>
       <ScreenHeader title='Perfil' />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            size={33}
-            source={{
-              uri: 'https://avatars.githubusercontent.com/u/95872678?v=4',
-            }}
-            alt='Imagem do usuário'
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded='full'
+              startColor='gray.400'
+              endColor='gray.300'
+            />
+          ) : (
+            <UserPhoto
+              size={PHOTO_SIZE}
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/95872678?v=4',
+              }}
+              alt='Imagem do usuário'
+            />
+          )}
         </Center>
       </ScrollView>
     </VStack>
