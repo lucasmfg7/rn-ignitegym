b1a5cd797f9b15e1ede717ef0413799dafafd6e4
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-10-13 11:46:22 -0300

    Foto do usuário

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,11 +1,23 @@
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from 'native-base'
+import { UserPhoto } from '@components/UserPhoto'
+import { Center, ScrollView, VStack } from 'native-base'
 import React from 'react'
 
 export const Profile = () => {
   return (
     <VStack>
       <ScreenHeader title='Perfil' />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            size={33}
+            source={{
+              uri: 'https://avatars.githubusercontent.com/u/95872678?v=4',
+            }}
+            alt='Imagem do usuário'
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
