cdb11087dd105ad7d47b42086bde8f396e74192e
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-10-13 11:08:16 -0300

    Componente HistoryCard

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,3 +1,4 @@
+import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { VStack } from 'native-base'
 
@@ -5,6 +6,7 @@ export const History = () => {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Histórico de Exercícios' />
+      <HistoryCard />
     </VStack>
   )
 }
