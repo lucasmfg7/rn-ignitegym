3152f1477ead6ecf9a5f9ccc66c4c9a46948acbf
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-10-13 11:02:32 -0300

    Componente ScreenHeader

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,10 +1,10 @@
-import { Center, Text } from 'native-base'
-import React from 'react'
+import { ScreenHeader } from '@components/ScreenHeader'
+import { VStack } from 'native-base'
 
 export const History = () => {
   return (
-    <Center flex={1}>
-      <Text>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title='Histórico de Exercícios' />
+    </VStack>
   )
 }
