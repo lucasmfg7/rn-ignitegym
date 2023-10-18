e43d24c7dafe48a22cc9f00ede89194e0c9ccc7a
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-10-18 17:37:46 -0300

    Exibindo a foto selecionada

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -19,6 +19,9 @@ const PHOTO_SIZE = 33
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [userPhoto, setuserPhoto] = useState(
+    'https://avatars.githubusercontent.com/u/95872678?v=4'
+  )
 
   async function handleUserPhotoSelection() {
     const selectedPhoto = await ImagePicker.launchImageLibraryAsync({
@@ -28,9 +31,9 @@ export function Profile() {
       allowsEditing: true,
     })
 
-    console.log(selectedPhoto)
-
     if (selectedPhoto.canceled) return
+
+    setuserPhoto(selectedPhoto.assets[0].uri)
   }
 
   return (
@@ -50,7 +53,7 @@ export function Profile() {
           ) : (
             <UserPhoto
               source={{
-                uri: 'https://avatars.githubusercontent.com/u/95872678?v=4',
+                uri: userPhoto,
               }}
               alt='Foto do usuário'
               size={PHOTO_SIZE}
