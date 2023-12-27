8abde2c3a021314b0c42447eeae573f255e11d59
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-12-26 22:19:05 -0300

    Redirecionar o usuário logado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,11 @@ import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { AuthRoutes } from './auth.routes'
 import { AppRoutes } from './app.routes'
 import { Box, useTheme } from 'native-base'
+import { useAuth } from '@hooks/useAuth'
 
 export const Routes = () => {
   const { colors } = useTheme()
+  const { user } = useAuth()
 
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
@@ -12,7 +14,7 @@ export const Routes = () => {
   return (
     <Box flex={1} bg='gray.700'>
       <NavigationContainer>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   )
